51b036adb80e14bd0b38a92e1e2600f00ba6e87f
ClainAtomi <clainatomi.ctb@gmail.com>
2014-10-15 13:42:32 +0300

    magic commit to app.php includes base logic by clain atomi

--- api/api.php
+++ api/api.php
@@ -4,14 +4,36 @@ class Api
     private $_requestType;
     private $_requestArgs;
     private $_requestUrl;
+    
+    private $_module;
+    
     public function __construct()
     {
         $this->_requestType = $_SERVER['REQUEST_METHOD'];
         $this->_requestArgs = $_REQUEST;
         $this->_requestUrl = $_SERVER['REQUEST_URI'];
-        echo $this->_requestType . '<br>';
-        print_r($this->_requestArgs);
-        echo $this->_requestUrl . '<br>';
+        $this->loadModuleByName($this->parseModuleName($this->_requestUrl);
     }
+    
+    private function parseModuleName($url)
+    {
+        $urlNodesList = explode('/', $url);
+        $moduleName = $urlNodesList[1];
+        return $moduleName;
+    }
+    
+    private function loadModuleByName($moduleName)
+    {
+        try
+        {
+            $this->_module = new $moduleName;
+        }
+        catch
+        {
+            $this->_module = new Module_404();
+            throw new Exeption('Module does not exists');
+        }
+    }
+    
 }
 ?>
