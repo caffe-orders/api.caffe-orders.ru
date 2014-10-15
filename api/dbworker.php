21fd3004156538605e1902c4c93abd543faf59ed
ClainAtomi <clainatomi.ctb@gmail.com>
2014-10-15 16:22:38 +0300

    makes dbworker singletone by clainatomi

--- api/dbworker.php
+++ api/dbworker.php
@@ -1,48 +1,37 @@
 <?php
 class DbWorker
 {
-    Public  $db;  
-    
-    public function __construct()
+    private static $instance = null;
+
+    private function __construct()
     {
-        $dsn =  DB_TYPE.":dbname=" . DB_NAME . ";host=" . DB_HOST;
-        try
+        //You shall not pass!
+    }
+
+    private function __clone()
+    {
+        //Me not like clones! Me smash clones!
+    }
+
+    public static function GetInstance()
+    {
+        if (!isset(self::$instance)) 
         {
-            $this->db = new PDO($dsn, DB_USER, DB_PASS);
-            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->db->query("set names utf8");
-        } 
-        catch (PDOException $e)
-        {            
-            echo 'Error : '.$e->getMessage();
-            exit();            
+            $dsn =  DB_TYPE.":dbname=" . DB_NAME . ";host=" . DB_HOST;
+            
+            try
+            {
+                self::$instance = new PDO($dsn, DB_USER, DB_PASS);
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$instance->query("set names utf8");
+            }
+            catch (PDOException $e)
+            {            
+                echo 'Error : '.$e->getMessage();
+                exit();            
+            }
         }
-    }
-    
-    public function insert($str,$array)
-    {  
-        $sth = $this->db->prepare($str);
-        $sth->execute($array);
-        return $this->db->lastInsertId();
-    }
-    
-    public function select($str,$array=null)
-    {
-        $sth = $this->db->prepare($str);
-        $sth->execute($array);
-        return $sth->fetchAll();
-    }
-    
-    public function delete($str,$array)
-    {
-        $sth = $this->db->prepare($str);
-        $sth->execute($array);
-    }
-    
-    public function update($str,$array)
-    {
-        $sth = $this->db->prepare($str);
-        $sth->execute($array);
+        return self::$instance;
     }
 }
 ?>
