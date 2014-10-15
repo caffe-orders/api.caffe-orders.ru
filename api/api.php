5f45bd2166a86d97c926b186afa551c556a74859
ClainAtomi <clainatomi.ctb@gmail.com>
2014-10-15 14:13:03 +0300

    last commit update by clainatomi add dbworker functionality

--- api/api.php
+++ api/api.php
@@ -5,14 +5,23 @@ class Api
     private $_requestArgs;
     private $_requestUrl;
     
+    private $_dbWorker;
     private $_module;
+    private $_accessLevel;
     
     public function __construct()
     {
         $this->_requestType = $_SERVER['REQUEST_METHOD'];
         $this->_requestArgs = $_REQUEST;
         $this->_requestUrl = $_SERVER['REQUEST_URI'];
-        $this->loadModuleByName($this->parseModuleName($this->_requestUrl);
+        $this->_initDbWorker();
+        $this->loadModuleByName($this->parseModuleName($this->_requestUrl));
+    }
+    
+    private function initDbWorker()
+    {
+        $dbWorkerInitAgrs = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+        $this->_dbWorker = new DbWorker($dbWorkerInitArgs, DB_USER, DB_PASS);
     }
     
     private function parseModuleName($url)
