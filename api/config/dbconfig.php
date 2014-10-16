6377f352f51fdee1d04efe466212947a0377f3af
broff <dvd2444@mail.ru>
2014-10-16 21:03:39 +0300

    add db connect values by broff

--- api/config/dbconfig.php
+++ api/config/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 //echo 'DBCONFIG';
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_HOST', "127.0.0.1");
-define('DB_NAME', "test_db");
+define('DB_USER', 'vh206740_apiUser');
+define('DB_PASS', '87654321');
+define('DB_HOST', "mysql.caffe-orders.ru");
+define('DB_NAME', "vh206740_apisql");
 define('DB_TYPE', "mysql");
