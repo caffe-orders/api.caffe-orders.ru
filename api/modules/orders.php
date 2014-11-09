8df37a35500f19eff5ea4040c463bfc2ae3c8367
broff <dvd2444@mail.ru>
2014-11-09 18:28:00 +0300

    Added function activate in module orders by broff

--- api/modules/orders.php
+++ api/modules/orders.php
@@ -141,17 +141,24 @@ class Orders extends Module implements Module_Interface
                         $result = $query->fetch();
                         if(!$result)
                         {
-                            $queryStr = 'INSERT orders (table_id, user_id, time, type, status, enter_time) 
-                                VALUES (:table_id, :user_id, :time, :type, :status, :enter_time)';                    
+                            $query = DbWorker::GetInstance()->prepare('UPDATE tables SET status = 1 WHERE id = :id');     
+                            $query->execute(array(':id' => (int)$args['table_id']));
+                            
+                            $queryStr = 'INSERT orders (table_id, user_id, time, type, status, enter_time, activated_code, activate_attempts) 
+                                VALUES (:table_id, :user_id, :time, :type, :status, :enter_time, :activated_code, :activate_attempts)';                    
                             $query = DbWorker::GetInstance()->prepare($queryStr);
                     
+                            $code = rand(0, 9999);
+                            
                             $queryArgsList = array(
                             ':table_id' => (int)$args['table_id'], 
                             ':user_id' => $userId,
                             ':time' => date('Y-m-d H:i:s'),
                             ':type' => 1,
                             ':status' => 1, 
-                            ':enter_time' => date('Y-m-d H:i:s')
+                            ':enter_time' => date('Y-m-d H:i:s'),
+                            ':activated_code' => $code,
+                            ':activate_attempts' => 0
                             );
                             if($query->execute($queryArgsList))
                             {
@@ -182,6 +189,160 @@ class Orders extends Module implements Module_Interface
                 $queryResponseData = array('err_code' => '602');
             }
             
+            return $queryResponseData;
+        });
+        
+        $this->get('newlong', 0, function($args)
+        {
+            $parametersArray = array(
+                'table_id',
+                'enter_time'
+            ); 
+            
+            if(Module::CheckFunctionArgs($parametersArray, $args))
+            {
+                $query = DbWorker::GetInstance()->prepare('SELECT * FROM tables WHERE id = :table_id');
+                $query->execute(array(':table_id' => (int)$args['table_id']));
+                $result = $query->fetch();
+                if($result)
+                {
+                    if((int)$result['status'] == 0)
+                    {
+                        $userId = (int)$_SESSION['id'];
+                        $query = DbWorker::GetInstance()->prepare('SELECT * FROM orders WHERE user_id = :user_id');
+                        $query->execute(array(':user_id' => $userId));
+                        $result = $query->fetch();
+                        if(!$result)
+                        {
+                            $query = DbWorker::GetInstance()->prepare('UPDATE tables SET status = 1 WHERE id = :id');     
+                            $query->execute(array(':id' => (int)$args['table_id']));
+                            
+                            $queryStr = 'INSERT orders (table_id, user_id, time, type, status, enter_time, activated_code, activate_attempts) 
+                                VALUES (:table_id, :user_id, :time, :type, :status, :enter_time, :activated_code, :activate_attempts)';                    
+                            $query = DbWorker::GetInstance()->prepare($queryStr);
+                            
+                            $code = rand(0, 9999);
+                            
+                            $queryArgsList = array(
+                            ':table_id' => (int)$args['table_id'], 
+                            ':user_id' => $userId,
+                            ':time' => date('Y-m-d H:i:s'),
+                            ':type' => 2,
+                            ':status' => 1, 
+                            ':enter_time' => date('Y-m-d H:i:s'),
+                            ':activated_code' => $code,
+                            ':activate_attempts' => 0
+                            );
+                            if($query->execute($queryArgsList))
+                            {
+                                $queryResponseData = array('err_code' => '200');
+                            }
+                            else
+                            {
+                                $queryResponseData = array('err_code' => '401','data' => 'Undefined error');
+                            }
+                        }
+                        else
+                        {
+                            $queryResponseData = array('err_code' => '401','data' => 'The user has already registered table');
+                        }
+                    }
+                    else
+                    {
+                        $queryResponseData = array('err_code' => '400', 'data' => 'Table busy');
+                    }    
+                }
+                else
+                {
+                    $queryResponseData = array('err_code' => '400', 'data' => 'Table in not found');
+                }
+            }
+            else
+            {                
+                $queryResponseData = array('err_code' => '602');
+            }
+            
+            return $queryResponseData;
+        });
+        
+        $this->get('activate', 1, function($args)
+        {
+            $requiredParams = array(
+                'code'
+            );
+           
+            $queryResponseData =  array();
+            if(Module::CheckFunctionArgs($requiredParams, $args))
+            {
+                $userId = (int)$_SESSION['id'];
+                $query = DbWorker::GetInstance()->prepare('SELECT * FROM orders WHERE user_id = :user_id');     
+                $query->execute(array(':user_id' => $userId));
+                $result = $query->fetch();
+                if($result)
+                {
+                    $timeNow = date('Y-m-d H:i:s', mktime(date("H"), date("i")-3, date("s"), date("m"), date("d"), date("Y")));
+                    $timeReg = $result['time'];
+                    if($timeNow > $timeReg)
+                    {
+                        $query = DbWorker::GetInstance()->prepare('UPDATE tables SET status = 0 WHERE id = :id');     
+                        $query->execute(array(':id' => $result['table_id']));
+                                    
+                        $query = DbWorker::GetInstance()->prepare('DELETE FROM orders WHERE user_id = :user_id');                
+                        $query->execute(array(':user_id' => $userId));
+                                
+                        $queryResponseData = array('err_code' => '603', 'data' => 'order deleted');
+                    }
+                    else
+                    {
+                        if($result['status'] == 1)
+                        {
+                            if($result['activated_code'] == $args['code'])
+                            {                        
+                                $query = DbWorker::GetInstance()->prepare('UPDATE tables SET status = 2 WHERE id = :id');     
+                                $query->execute(array(':id' => (int)$result['table_id']));
+                        
+                                $query = DbWorker::GetInstance()->prepare('UPDATE orders SET status = 2 WHERE user_id = :user_id');     
+                                $query->execute(array(':user_id' => $userId));
+                        
+                                $queryResponseData = array('err_code' => '200'); 
+                            }
+                            else
+                            {
+                                if($result['activate_attempts'] == 2)
+                                {
+                                    $query = DbWorker::GetInstance()->prepare('UPDATE tables SET status = 0 WHERE id = :id');     
+                                    $query->execute(array(':id' => $result['table_id']));
+                                    
+                                    $query = DbWorker::GetInstance()->prepare('DELETE FROM orders WHERE user_id = :user_id');                
+                                    $query->execute(array(':user_id' => $userId));
+                                
+                                    $queryResponseData = array('err_code' => '603', 'data' => 'order deleted');
+                                }
+                                else
+                                {
+                                    $attempts = (int)$result['activate_attempts'] + 1;
+                                    $query = DbWorker::GetInstance()->prepare('UPDATE orders SET activate_attempts = :attempts WHERE user_id = :user_id');     
+                                    $query->execute(array(':attempts' => $attempts,':user_id' => $userId));
+                            
+                                    $queryResponseData = array('err_code' => '603', 'data' => 'Wrong code');
+                                }                            
+                            }
+                        }
+                        else
+                        {
+                            $queryResponseData = array('err_code' => '200');
+                        }
+                    }
+                }
+                else
+                {
+                    $queryResponseData = array('err_code' => '604');
+                }   
+            }
+            else
+            {
+                $queryResponseData = array('err_code' => '602');
+            }
             return $queryResponseData;
         });
         
